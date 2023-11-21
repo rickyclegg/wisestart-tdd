05d5bf15d3e5247e6bc2830552f34ebac3825434
Ricky Clegg <ricky.clegg@transferwise.com>
2023-11-21 20:16:40 +0000

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { Kafka, logLevel, Partitioners } from 'kafkajs'
 import { BATTLE_ENDED_TOPIC, POKEMON_EVOLVED_TOPIC } from './topics'
 import debug from 'debug'
+import axios from 'axios';
 
 const log = debug('pokemon-evolver')
 
 const KAFKA_HOST = process.env.KAFKA_HOST as string
 const CLIENT_ID = 'pokemon-evolver'
 const GROUP_ID = `${CLIENT_ID}-${Math.floor(Math.random() * 1000)}`
+const POKE_API_URL = 'http://localhost:4000'
 
 ;(async () => {
   log(`Connecting to Kafka: ${KAFKA_HOST}`)
@@ -36,8 +38,16 @@ const GROUP_ID = `${CLIENT_ID}-${Math.floor(Math.random() * 1000)}`
   })
 
   await consumer.run({
-    eachMessage: async ({ topic }) => {
+    eachMessage: async ({ topic, message }) => {
       log(`Message received from Kafka: ${topic}`)
+
+      const messageValue = JSON.parse(<string>message.value?.toString())
+
+      const pokemon = await axios.get(`${POKE_API_URL}/pokemon/${messageValue.id}`)
+      const species = await axios.get(pokemon.data.species.url)
+      const evolutionChain = await axios.get(species.data.evolution_chain.url)
+
+
 
       log(`Send message >>> ${POKEMON_EVOLVED_TOPIC}`)
       await producer.send({
